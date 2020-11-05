e7b0d18bef60f104e3af3f6576d73c42e813a145
Lint Action <lint-action@samuelmeuli.com>
2020-11-05 00:13:31 +0000

    Fix code style issues with Black

--- tests/test_pytorchobjective.py
+++ tests/test_pytorchobjective.py
@@ -146,34 +146,32 @@ def test_knitro():
         net.fc2.weight[0][1] = 1.0
         net.fc2.bias[0] = 0.00
 
-    torchCGen = torch.random.manual_seed(12345) 
+    torchCGen = torch.random.manual_seed(12345)
     data = torch.rand(1000, 1)
     data_loader = torch.utils.data.DataLoader(data)
     obj = PyTorchObjective(loss, net, data_loader)
 
     try:
-        kc = KN_new ()
+        kc = KN_new()
     except:
-        print ("Failed to find a valid license.")
-        quit ()
-    KN_add_vars (kc, 5)
-    KN_set_var_primal_init_values (kc, xInitVals = obj.x0)
-    cb = KN_add_eval_callback(kc, evalObj = True, funcCallback = obj.eval_f)
-    KN_set_cb_grad (kc, cb, objGradIndexVars = KN_DENSE, gradCallback = obj.eval_g)
-    KN_set_obj_goal (kc, KN_OBJGOAL_MINIMIZE)
+        print("Failed to find a valid license.")
+        quit()
+    KN_add_vars(kc, 5)
+    KN_set_var_primal_init_values(kc, xInitVals=obj.x0)
+    cb = KN_add_eval_callback(kc, evalObj=True, funcCallback=obj.eval_f)
+    KN_set_cb_grad(kc, cb, objGradIndexVars=KN_DENSE, gradCallback=obj.eval_g)
+    KN_set_obj_goal(kc, KN_OBJGOAL_MINIMIZE)
     # KN_set_int_param (kc, KN_PARAM_DERIVCHECK, KN_DERIVCHECK_ALL)
     # KN_set_int_param (kc, KN_PARAM_DERIVCHECK, KN_DERIVCHECK_ALL)
-    nStatus = KN_solve (kc)
+    nStatus = KN_solve(kc)
 
     print(Solution(kc))
 
     assert 1 == 1
-    
+
     # print(obj.fun(np.array([-0.05037972, -0.05037972,  0.71964827,  0.71964827, -0.55942466])))
     # obj.cache_argument(np.array([-0.013799221627639344, -0.013799221627639344, 0.8606748756170269, 0.8606748756170269, -0.278378617923651]))
     # print(obj.fun(np.array([-0.013799221627639344, -0.013799221627639344, 0.8606748756170269, 0.8606748756170269, -0.278378617923651])))
-    
-    
 
     sol = Solution(kc)
 
@@ -181,7 +179,8 @@ def test_knitro():
     print("x is", sol.x)
 
     obj.cache_argument(np.array(sol.x))
-    KN_free (kc)
+    KN_free(kc)
+
 
 # test_pytorch_obj()
 # # test_fake_class_knitro()
